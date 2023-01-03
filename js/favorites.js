ea934aa22bc2b649ee384b3daf7e9e3b1ed0e689
Victor Miguel Vilela Fernandes <113549997+victormiguelVF@users.noreply.github.com>
2023-01-03 16:32:29 -0300

    Fim do projeto!

--- js/favorites.js
+++ js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(({ login, name, public_repos, followers }) => ({
-            login,
-            name,
-            public_repos,
-            followers
-        }))
-    }
-}
+import { GithubUser } from "./githubuser.js"
 
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
@@ -27,11 +14,40 @@ export class Favorites {
         this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || []
     }
 
+    save() {
+        localStorage.setItem('@github-favorites', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado.')
+            }
+
+            const user = await GithubUser.search(username) 
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            
+        } catch(error) {
+            alert(error.message)
+        }
+        
+    }
+
     delete(user) {
         const filteresEntries = this.entries.filter(entry => entry.login !== user.login)
 
         this.entries = filteresEntries
         this.update()
+        this.save()
     }
 }
 
@@ -43,6 +59,16 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const {value} = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
@@ -53,6 +79,7 @@ export class FavoritesView extends Favorites {
 
         row.querySelector('.user img'). src = `https://github.com/${user.login}.png`
         row.querySelector('.user img').alt = `Imagem de ${user.name}`
+        row.querySelector('.user a').href = `https://github.com/${user.login}`
         row.querySelector('.user p').textContent = user.name
         row.querySelector('.user span').textContent = user.login
         row.querySelector('.repositories').textContent = user.public_repos
